90847b80a3bea641b867f249acd1770ce24e53d6
Karthik K Sayyaparaju <karthik.k.sayyaparaju@gmail.com>
2018-07-06 23:13:54 -0400

    Added Pyhton basic learning classes

---
+++ python_learning/learning_1.py
@@ -0,0 +1,38 @@
+
+####Print Commands
+
+#name = input("Please enter your name: ")
+
+#### Input command to learn
+#age= int(input("Hi {0}, Please enter your age : ".format(name)))
+
+####IF condition to learn
+#if age < 18 | age > 31:
+
+#    print("Sorry, you are not eliglible for Holidays")
+#else :
+
+#### Print vlaue of
+#    print("Congratulations {0}, you are welcome to the holidays".format(name))
+
+
+parrot = 'Indian'
+print(parrot[0:5:2])
+
+print("karthik" in "karthik")
+
+print("Printing months {0}, {1}, {2}".format("january","Feb","March"))
+
+#### %d and %s learning
+print("My age is %s %d, %d %d" %("karthik",6,5,6))
+
+#### %d and %s with paces and level of precision neede
+print("My age is %s %2d, %10d %d" %("karthik",6,5,6))
+print("Value of pi is %12.59f"%(22/7))
+print("Value of pi is {0:12.50}".format(22/7))
+print("My name is ")
+
+a= "Aruna"
+b="Banglore"
+c=str(19)
+print(a + b + c )
